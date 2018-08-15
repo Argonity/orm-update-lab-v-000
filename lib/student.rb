a1e6ecf5a47934ba81e260d37e888ff4038b48ac
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-15 10:54:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,14 +29,16 @@ class Student
   end
 
   def save
-  if self.id
-    self.update
-  else
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
     DB[:conn].execute(sql, self.name, self.grade)
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
@@ -55,19 +57,19 @@ class Student
   end
 
   def self.new_from_db(row)
-      id = row[0]
-      name = row[1]
-      grade = row[2]
-      self.new(id, name, grade)  # self.new is the same as running Song.new
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)  # self.new is the same as running Song.new
   end
 
   def self.find_by_name(name)
- sql = <<-SQL
-   SELECT *
-   FROM students
-   WHERE name = ?
-   --LIMIT 1
- SQL
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE name = ?
+     --LIMIT 1
+   SQL
 
  DB[:conn].execute(sql, name).map do |row|
    self.new_from_db(row)
